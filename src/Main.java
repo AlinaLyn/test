eb0af39662be8063c558f9439163e86079dfb679
AlinaLyn <alina.lyn@ukr.net>
2023-03-03 09:51:01 +0200

    solved

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        int a=10 ;
+        int b=3 ;
+        double c =(double)a/b ;
+        System.out.println(c);
+
+        int d =a%b;
+        System.out.println(d);
+        // task2
+        int g = 60;
+        System.out.println((g/10)+(g%10));
+        //task3
+        double a1= 28.3;
+        System.out.println((int)a1);
+
+
+
+
+    }
+}
